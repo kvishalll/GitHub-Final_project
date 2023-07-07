0263c5999dc36a613411d097198df6982fc643e6
Vishal Kumar <101967158+kvishalll@users.noreply.github.com>
2023-07-07 19:13:33 +0530

    Update kvishall.sh

--- kvishall.sh
+++ kvishall.sh
@@ -1,1 +1,8 @@
 
+principle=int(input("Enter your Amount :"))
+Time=int(input("enter time taken :"))
+rate=int(input("enter your rate of interest :"))
+
+simple_interest=principle*Time*rate/100
+
+print("your interest will be :",simple_interest)
